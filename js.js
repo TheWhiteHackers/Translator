765281a11326b638830f0da23c2426e0ec5dddcd
TheWhiteHackers <nurefsanbera@gmail.com>
2022-12-18 14:59:10 -0600

    almost there

--- js.js
+++ js.js
@@ -31,5 +31,37 @@ btnTranslate.addEventListener("click", ()=>{
 //text-to-speech
 const fromvol = document.getElementById("volumefrom");
 const tovol = document.getElementById("volumeto");
+const synth = window.speechSynthesis;
 
-// fromvol.addEventListener()
+fromvol.addEventListener("click", function(e){
+    e.preventDefault();
+    window.navigator.clipboard.writeText(fromText.value);
+    console.log(fromText.value);
+    let utterance;
+    utterance = new SpeechSynthesisUtterance(fromText.value);
+    utterance.lang = fromLang.value;
+    speechSynthesis.speak(utterance);
+});
+
+tovol.addEventListener("click", function(e){
+    e.preventDefault();
+    window.navigator.clipboard.writeText(toText.value);
+    console.log(toText.value);
+    let utterance;
+    utterance = new SpeechSynthesisUtterance(toText.value);
+    utterance.lang = toLang.value;
+    speechSynthesis.speak(utterance);
+})
+
+//copy
+const fromcopy = document.getElementById("fromcopy");
+const tocopy = document.getElementById("tocopy");
+
+fromcopy.addEventListener("click", function(e){
+    e.preventDefault();
+    console.log("Here is the part you copy:  " + fromText.value);
+})
+tocopy.addEventListener("click", function(e){
+    e.preventDefault();
+    console.log("Here is the part you copy:  " + toText.value);
+})
